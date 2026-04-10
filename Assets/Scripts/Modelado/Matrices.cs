2ba5565f8c5dfb9d263089cd98a9989fa7bfbb38
grosso <4872133+GrossoDev@users.noreply.github.com>
2026-04-10 14:06:03 -0300

    Refactor de Matrices.cs

--- Assets/Scripts/Modelado/Matrices.cs
+++ Assets/Scripts/Modelado/Matrices.cs
@@ -4,46 +4,46 @@ using UnityEngine;
 
 public class Matrices
 {
-    private Matrix4x4 CreateModelMatrix(Vector3 newPosition, Vector3 newRotation, Vector3 newScale)
+    public static Matrix4x4 CreateModelMatrix(Vector3 newPosition, Vector3 newRotation, Vector3 newScale)
     {
         Matrix4x4 positionMatrix = new Matrix4x4(
             new Vector4(1f, 0f, 0f, newPosition.x), //Primera columna
             new Vector4(0f, 1f, 0f, newPosition.y),
             new Vector4(0f, 0f, 1f, newPosition.z),
-            new Vector4(0f, 0f, 0f, 1f)
+            new Vector4(0f, 0f, 0f,            1f)
         );
 
         positionMatrix = positionMatrix.transpose;
 
         Matrix4x4 rotationMatrixX = new Matrix4x4(
-            new Vector4(1f, 0f, 0f, 0f),
+            new Vector4(1f,                       0f,                        0f, 0f),
             new Vector4(0f, Mathf.Cos(newRotation.x), -Mathf.Sin(newRotation.x), 0f),
-            new Vector4(0f, Mathf.Sin(newRotation.x), Mathf.Cos(newRotation.x), 0f),
-            new Vector4(0f, 0f, 0f, 1f)
+            new Vector4(0f, Mathf.Sin(newRotation.x),  Mathf.Cos(newRotation.x), 0f),
+            new Vector4(0f,                       0f,                        0f, 1f)
         );
 
         Matrix4x4 rotationMatrixY = new Matrix4x4(
-            new Vector4(Mathf.Cos(newRotation.y), 0f, Mathf.Sin(newRotation.y), 0f),
-            new Vector4(0f, 1f, 0f, 0f),
+            new Vector4( Mathf.Cos(newRotation.y), 0f, Mathf.Sin(newRotation.y), 0f),
+            new Vector4(                       0f, 1f,                       0f, 0f),
             new Vector4(-Mathf.Sin(newRotation.y), 0f, Mathf.Cos(newRotation.y), 0f),
-            new Vector4(0f, 0f, 0f, 1f)
+            new Vector4(                       0f, 0f,                       0f, 1f)
         );
 
         Matrix4x4 rotationMatrixZ = new Matrix4x4(
             new Vector4(Mathf.Cos(newRotation.z), -Mathf.Sin(newRotation.z), 0f, 0f),
-            new Vector4(Mathf.Sin(newRotation.z), Mathf.Cos(newRotation.z), 0f, 0f),
-            new Vector4(0f, 0f, 1f, 0f),
-            new Vector4(0f, 0f, 0f, 1f)
+            new Vector4(Mathf.Sin(newRotation.z),  Mathf.Cos(newRotation.z), 0f, 0f),
+            new Vector4(                      0f,                        0f, 1f, 0f),
+            new Vector4(                      0f,                        0f, 0f, 1f)
         );
 
         Matrix4x4 rotationMatrix = rotationMatrixZ * rotationMatrixY * rotationMatrixX;
         rotationMatrix = rotationMatrix.transpose;
 
         Matrix4x4 scaleMatrix = new Matrix4x4(
-            new Vector4(newScale.x, 0f, 0f, 0f),
-            new Vector4(0f, newScale.y, 0f, 0f),
-            new Vector4(0f, 0f, newScale.z, 0f),
-            new Vector4(0f, 0f, 0f, 1f)
+            new Vector4(newScale.x,         0f,         0f, 0f),
+            new Vector4(        0f, newScale.y,         0f, 0f),
+            new Vector4(        0f,         0f, newScale.z, 0f),
+            new Vector4(        0f,         0f,         0f, 1f)
         );
 
         scaleMatrix = scaleMatrix.transpose;
@@ -54,7 +54,7 @@ public class Matrices
         return finalMatrix;
     }
 
-    private Matrix4x4 CreateViewMatrix(Vector3 pos, Vector3 target, Vector3 up)
+    public static Matrix4x4 CreateViewMatrixFromTargetPoint(Vector3 pos, Vector3 target, Vector3 up)
     {
         // Target apunta al objeto a ver y Pos apunta a la cámara
         // Target-pos apunta al objeto desde la cámara
@@ -66,36 +66,45 @@ public class Matrices
 
         Matrix4x4 vista = new Matrix4x4
         (
-            new Vector4(right.x, newUp.x, -forward.x, 0f),
-            new Vector4(right.y, newUp.y, -forward.y, 0f),
-            new Vector4(right.z, newUp.z, -forward.z, 0f),
+            new Vector4(                 right.x,                  newUp.x,                -forward.x, 0f),
+            new Vector4(                 right.y,                  newUp.y,                -forward.y, 0f),
+            new Vector4(                 right.z,                  newUp.z,                -forward.z, 0f),
             new Vector4(-Vector3.Dot(right, pos), -Vector3.Dot(newUp, pos), Vector3.Dot(forward, pos), 1f)
         );
         return vista;
     }
 
-    private Matrix4x4 CreateProjectionMatrix(float fov, float aspect, float nearClip, float farClip)
+    public static Matrix4x4 CreateViewMatrixFromSphericalCoords(Vector3 pos, Vector2 pitchYaw)
+    {
+        // TODO: implementar esto
+
+        return new Matrix4x4();
+    }
+
+    public static Matrix4x4 CreateProjectionMatrix(float fov, float aspect, float nearClip, float farClip)
     {
         var invTan = 1f / Mathf.Tan(Mathf.Deg2Rad * fov / 2);
         var diffNearFar = nearClip - farClip;
 
-        return new Matrix4x4(
+        return GL.GetGPUProjectionMatrix(new Matrix4x4(
             new Vector4(invTan / aspect,                  0,                                    0,                                  0),
             new Vector4(              0,             invTan,                                    0,                                  0),
             new Vector4(              0,                  0,   (nearClip + farClip) / diffNearFar,   2*nearClip*farClip / diffNearFar),
             new Vector4(              0,                  0,                                   -1,                                  0)
-        );
+        ).transpose, true);
     }
 
     public void RecalcularMatrices(Vector3 newPosition, Vector3 newRotation, Vector3 newScale, Vector3 cameraPos, Vector3 target, Vector3 up, GameObject gameObject)
     {
         Matrix4x4 modelMatrix, viewMatrix, projMatrix;
+        Material material = gameObject.GetComponent<Renderer>().material;
 
         modelMatrix = CreateModelMatrix(newPosition, newRotation, newScale);
-        gameObject.GetComponent<Renderer>().material.SetMatrix("_ModelMatrix", modelMatrix);
-        viewMatrix = CreateViewMatrix(cameraPos, target, up);
-        gameObject.GetComponent<Renderer>().material.SetMatrix("_ViewMatrix", viewMatrix);
+        viewMatrix = CreateViewMatrixFromTargetPoint(cameraPos, target, up);
         projMatrix = CreateProjectionMatrix(60, 16f/9f, 0.01f, 100f);
-        gameObject.GetComponent<Renderer>().material.SetMatrix("_ProjectionMatrix", GL.GetGPUProjectionMatrix(projMatrix.transpose, true));
+
+        material.SetMatrix("_ModelMatrix", modelMatrix);
+        material.SetMatrix("_ViewMatrix", viewMatrix);
+        material.SetMatrix("_ProjectionMatrix", projMatrix);
     }
 }
