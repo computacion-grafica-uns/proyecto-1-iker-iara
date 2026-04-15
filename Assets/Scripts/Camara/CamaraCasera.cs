46808c0357146176d09c5316135e9dc420314bb3
Grosso <4872133+GrossoDev@users.noreply.github.com>
2026-04-15 09:52:40 -0300

    CamaraCasera ahora calcula automaticamente el aspect al iniciar

--- Assets/Scripts/Camara/CamaraCasera.cs
+++ Assets/Scripts/Camara/CamaraCasera.cs
@@ -4,9 +4,16 @@ public abstract class CameraCasera : MonoBehaviour
 {
     public Vector3 position;
     public float aspectRatio = 16f/9f;
+    public bool calculateAspectOnLoad = true;
     public float fov = 60f;
     public float nearClip = 0.1f;
     public float farClip = 1000f;
+
+    void Awake()
+    {
+        if (calculateAspectOnLoad)
+            aspectRatio = Screen.width / Screen.height;
+    }
 
     public abstract Matrix4x4 GetViewMatrix();
 
